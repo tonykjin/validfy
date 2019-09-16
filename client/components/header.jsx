ab00e8e413402bb325a3b247a23a339a6d446872
tonykjin <tonyjin23@gmail.com>
2019-09-16 04:59:18 +0000

    achieved basic layout for header component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -10,10 +10,13 @@ export default class Header extends React.Component {
             timeout={200}
             classNames='fade container'>
                 <div className="header banner row-fluid">
-                    <div className="col-2" id="logo"></div>
-                    <div className="col-2 hButton">Home</div>
-                    <div className="col-2 hButton">About</div>
-                    <div className="col-2 hButton">Team</div>
+                    <div className="col" id="logo"></div>
+                    <div className="col logo-text">Validfy</div>
+                    <div className="col hButton">Home</div>
+                    <div className="col hButton">About</div>
+                    <div className="col hButton">Team</div>
+                    <div className="col hButton">Sign Up</div>
+                    <div className="col hButton">Log In</div>
                 </div>
             </CSSTransition>
         )
