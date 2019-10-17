8a0fcd90f5f93f5f3a6ad495c4bf7dc21017e801
Tony Jin <tonyjin23@gmail.com>
2019-10-17 15:44:38 -0700

    created rudimentary login screen via next

---
+++ client/components/main/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+import Header from '../../pages/header';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ client/pages/header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+import { Button, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="light" variant="light">
+      <Link className="mr-auto" href="../components/main/app">
+        <img src="../public/styles/img/logo_notext.png"></img>
+      </Link>
+      <Link className="mr-auto" href="/about">
+        <a>about</a>
+      </Link>
+      <Link className="mr-auto" href="/login">
+        <a>login</a>
+      </Link>
+      <Link className="mr-auto" href="/signup">
+        <a>sign up</a>
+      </Link>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
